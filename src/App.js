009c47aa83beca1c450a9d73493ec329150269ea
prabhat pankaj <pankajiitbhu@gmail.com>
2020-12-30 19:39:49 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -12,6 +12,29 @@ const App = () => {
   );
   const  [convertedContent, setConvertedContent] = useState(null);
 
+  function uploadImageCallBack(file) {
+    return new Promise(
+      (resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://api.imgur.com/3/image');
+        xhr.setRequestHeader('Authorization', 'Client-ID ##clientid###');
+        const data = new FormData();
+        data.append('image', file);
+        xhr.send(data);
+        xhr.addEventListener('load', () => {
+          const response = JSON.parse(xhr.responseText);
+          console.log(response)
+          resolve(response);
+        });
+        xhr.addEventListener('error', () => {
+          const error = JSON.parse(xhr.responseText);
+          console.log(error)
+          reject(error);
+        });
+      }
+    );
+  }
+
   const handleEditorChange = (state) => {
     setEditorState(state);
     convertContentToHTML();
@@ -39,6 +62,14 @@ const App = () => {
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
+        toolbar={{
+          inline: { inDropdown: true },
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: true },
+          image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
+        }}
       />
       <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>
     </div>
